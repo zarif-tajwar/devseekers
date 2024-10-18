17ee7fef580a489f644887bb08f46d9b040f698f
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-10-18 19:51:39 +0600

    feat: add runtime validated and typesafe env vars in backend
    * exports a function 'getEnvValue' to provide typesafe env consumption

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
+import { Module } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
+import { loadEnv } from "./config/env.config";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [loadEnv],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/config/env.config.ts
@@ -0,0 +1,40 @@
+import { ConfigService, registerAs } from "@nestjs/config";
+import { z } from "zod";
+import { fromZodError } from "zod-validation-error";
+
+/**
+ * Zod schema for validating env vars
+ * NOTE: All env vars must be registered inside this object!!
+ */
+export const envSchema = z.object({
+  FRONTEND_URL: z.string(),
+});
+
+/**
+ * Loads the env vars from envSchema and validates them
+ * Throws an error if a required env var is missing
+ */
+export const loadEnv = registerAs("envConfig", () => {
+  const parsed = envSchema.safeParse(process.env);
+
+  if (parsed.error)
+    throw new Error(
+      `Invalid Environment Variables: ${fromZodError(parsed.error)}`,
+    );
+
+  return parsed.data;
+});
+
+/**
+ * Env vars types
+ */
+export type EnvConfigType = z.infer<typeof envSchema>;
+
+/**
+ * Gets env value using ConfigService with type generics
+ * Provides an typesafe way to consume env vars
+ */
+export const getEnvValue = <TEnvKey extends keyof EnvConfigType>(
+  configService: ConfigService,
+  envKey: TEnvKey,
+) => configService.get(envKey) as EnvConfigType[TEnvKey];
