73f6d73ec216531512a6e86aa59835c3f97c460e
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-10-30 01:22:23 +0600

    chore(eslint-config): allow underscore placeholders from no-unused-vars rule

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -18,4 +18,19 @@ module.exports = {
     "coverage/",
     "node_modules/",
   ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 };
