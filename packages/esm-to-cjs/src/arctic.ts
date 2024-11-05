f0d307fd9421a7d2c0c07c0c4fef5ba1bbbeb79c
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-11-05 21:54:03 +0600

    [backend] feat: add session service to create and control user sessions
    * adds prefixes for redis session keys
    * adds session service that includes session creation, validation and invalidation
    * adds dayjs to handle time/date related operations
    * seperates oslo and arctic

---
+++ packages/esm-to-cjs/src/arctic.ts
@@ -0,0 +1,11 @@
+export {
+  GitHub,
+  Google,
+  Facebook,
+  LinkedIn,
+  Twitter,
+  generateState,
+  generateCodeVerifier,
+  decodeIdToken,
+  OAuth2Tokens,
+} from "arctic/dist";
--- packages/esm-to-cjs/src/oslo.ts
+++ packages/esm-to-cjs/src/oslo.ts
@@ -4,5 +4,3 @@ export {
 } from "@oslojs/encoding";
 
 export { sha256 } from "@oslojs/crypto/sha2";
-
-export { GitHub, Google, Facebook, LinkedIn, Twitter } from "arctic/dist";
