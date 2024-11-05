5475d0725a2883e254ebc0a6bfb9cfc89de4f131
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-11-05 21:11:22 +0600

    [backend] refactor: update env var extraction to make turbo repo eslint aware of backend env vars
    * also removes mistaken https from .env.example
    * adds session expiry time env
    * adds also env vars in root turbo json so that turborepo can keep track of them

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -1,8 +1,14 @@
 import { z } from "zod";
 
 /**
+ * (NOTE)
+ * Check here to see an usage example: https://github.com/devseekers/devseekers/blob/main/docs/examples/BACKEND_USAGE_EXAMPLES.md#add-and-use-environment-variables-in-nestjs-backend-with-typesafety
+ */
+
+/**
  * Zod schema for validating env vars
- * NOTE: All env vars must be registered inside this object!!
+ * (NOTE)
+ * All env vars validators must be inside this object
  */
 export const envSchema = z.object({
   FRONTEND_URL: z.string().url(),
@@ -10,4 +16,19 @@ export const envSchema = z.object({
   ADMIN_FRONTEND_URL: z.string().url(),
   REDIS_URI: z.string().url(),
   POSTGRES_URI: z.string().url(),
+  SESSION_EXPIRY_TIME_IN_DAYS: z.coerce.number(),
+});
+
+/**
+ * (NOTE)
+ * All env vars must be registered here
+ * and also to the root turbo.json global Env
+ */
+export const extractEnvValues = () => ({
+  FRONTEND_URL: process.env.FRONTEND_URL,
+  PORT: process.env.PORT,
+  ADMIN_FRONTEND_URL: process.env.ADMIN_FRONTEND_URL,
+  REDIS_URI: process.env.REDIS_URI,
+  POSTGRES_URI: process.env.POSTGRES_URI,
+  SESSION_EXPIRY_TIME_IN_DAYS: process.env.SESSION_EXPIRY_TIME_IN_DAYS,
 });
