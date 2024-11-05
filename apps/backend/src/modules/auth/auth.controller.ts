36397b185813439726c7314152738726e3df0db8
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-11-06 00:19:21 +0600

    [backend] feat: add logout and validate endpoints
    * also renames user decorator from "User" to "GetUser"

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthGuard } from "./guards/auth.guard";
+import { RequestWithUser } from "./interfaces/request-with-user.interface";
+import { Response } from "express";
+import { GetUser } from "./decorators/get-user.decorator";
+import { User } from "@repo/shared-lib/types/auth/user";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  /**
+   * Logs out by removing sessions from db and client cookies
+   */
+  @Get("logout")
+  @UseGuards(AuthGuard)
+  async logout(
+    @Req() req: RequestWithUser,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    await this.authService.invalidateSession(req.sessionId, req.user.id);
+    res.clearCookie("session");
+  }
+
+  /**
+   * Returns the user object
+   * if the request is authorized
+   */
+  @Get("validate")
+  @UseGuards(AuthGuard)
+  async validate(@GetUser() user: User) {
+    return user;
+  }
 }
--- apps/backend/src/modules/auth/decorators/get-user.decorator.ts
+++ apps/backend/src/modules/auth/decorators/get-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { RequestWithUser } from "../interfaces/request-with-user.interface";
 
-export const User = createParamDecorator(
+export const GetUser = createParamDecorator(
   (_: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest() as RequestWithUser;
     return request.user;
